57f0fe8c985eaf0dedbc5cbcea1ea4fb9c87c727
topepo <mxkuhn@gmail.com>
2018-07-21 21:39:36 -0400

    added a few more verbs based on emails

--- R/generics.R
+++ R/generics.R
@@ -4,38 +4,71 @@
 #' package dependencies when multiple packages have the same
 #' method.
 #'
+#' An attempt was made to use S3 class signatures that were
+#'  consistent with existing code. For example, \pkg{dplyr} and \pkg{lime}
+#'  both have `explain` methods that do very different things but
+#'  both take `x` as their main object. Even though `x` would be
+#'  different in those cases, this package provides access to the S3
+#'  generic so that other packages that may want to create `explain`
+#'  methods for their objects can do so without loading either of
+#'  the other packages.
+#' 
 #' For example, if a new `tidy` method is being developed for a package, this
 #'  lightweight package can be the required dependency to have access to the
-#'  generic method (instead of depending on `broom` and installing its
+#'  generic method (instead of depending on \pkg{broom} and installing its
 #'  dependencies).
+#'  
+#' In this package, snake case is preferred. The generics `varimp` and `varImp`
+#'  are provided to maintain compatability with existing pacakges (e.g. `party` 
+#'  and `caret`). Both refer to the `var_imp` 
+#'  
+#' Known non-S4 functions related to modeling (which may or may not be 
+#' consistent with the definitions in this package):
+#' 
+#' \itemize{
+#' \item `augment`: \pkg{broom}
+#' \item `compile`: \pkg{keras}, \pkg{tensorflow}
+#' \item `estfun`: \pkg{sandwich}, \pkg{ssym}, \pkg{gmm}, \pkg{maxLik}, 
+#'   \pkg{psychotools}, \pkg{drc}, and others
+#' \item `evaluate`: \pkg{keras}, \pkg{tensorflow}, \pkg{healthcareai}
+#' \item `explain`: \pkg{dplyr}, \pkg{lime}, \pkg{DALEX}
+#' \item `fit`: \pkg{keras}
+#' \item `glance`: \pkg{broom}
+#' \item `learn`: \pkg{grnn}, \pkg{pnn}
+#' \item `prune`: \pkg{rpart}, \pkg{dendextend}
+#' \item `tidy`: \pkg{broom}, \pkg{estimatr}, \pkg{radiant.data}, 
+#'  \pkg{permutations}, and others. 
+#' \item `train`: \pkg{caret}, \pkg{tensorflow}, \pkg{mlr}, \pkg{RSNNS},
+#'  \pkg{Information}, and others. 
+#' \item `varImp`: \pkg{caret}, {datafsm}
+#' \item `varimp`: \pkg{party}, \pkg{partykit} , \pkg{mboost}, \pkg{semtree}
+#' \item `var_imp`: \pkg{datafsm}
+#' }
 #'
-#' @param object,x An object.
+#' @param object,x An object. See Details below. 
 #' @param data A data set in a data frame or tibble.
 #' @param tree A fitted model object.
 #' @param ... Additional arguments to pass to other functions.
 #' @examples
 #' fit
 #'
-#' train
-#' @export
-#' @rdname generics
-fit <- function (object, ...)
-  UseMethod("fit")
+#' tidy
+#' 
 
 #' @export
 #' @rdname generics
-var_imp <- function (object, ...)
-  UseMethod("var_imp")
+augment <- function(x, data, ...)
+  UseMethod("augment")
 
 #' @export
 #' @rdname generics
-train <- function (x, ...)
-  UseMethod("train")
+compile <- function (object, ...)
+  UseMethod("compile")
 
 #' @export
 #' @rdname generics
-learn <- function (x, ...)
-  UseMethod("learn")
+estfun <- function (x, ...)
+  UseMethod("estfun")
 
 #' @export
 #' @rdname generics
@@ -44,15 +77,13 @@ evaluate <- function (x, ...)
 
 #' @export
 #' @rdname generics
-prune <- function (tree, ...)
-  UseMethod("prune")
-
-### generics used by broom
+explain <- function (x, ...)
+  UseMethod("explain")
 
 #' @export
 #' @rdname generics
-tidy <- function (x, ...)
-  UseMethod("tidy")
+fit <- function (object, ...)
+  UseMethod("fit")
 
 #' @export
 #' @rdname generics
@@ -61,7 +92,35 @@ glance <- function(x, ...)
 
 #' @export
 #' @rdname generics
-augment <- function(x, data, ...)
-  UseMethod("augment")
+learn <- function (x, ...)
+  UseMethod("learn")
 
+#' @export
+#' @rdname generics
+prune <- function (tree, ...)
+  UseMethod("prune")
 
+#' @export
+#' @rdname generics
+tidy <- function (x, ...)
+  UseMethod("tidy")
+
+#' @export
+#' @rdname generics
+train <- function (x, ...)
+  UseMethod("train")
+
+#' @export
+#' @rdname generics
+var_imp <- function (object, ...)
+  UseMethod("var_imp")
+
+#' @export
+#' @rdname generics
+varimp <- function (object, ...)
+  UseMethod("var_imp")
+
+#' @export
+#' @rdname generics
+varImp <- function (object, ...)
+  UseMethod("var_imp")
