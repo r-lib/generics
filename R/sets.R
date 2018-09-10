aed2cad26bf35a98bc3baeb420284f926559b6a5
hadley <h.wickham@gmail.com>
2018-09-10 16:24:55 -0500

    Add set operation generics and default methods
    Fixes #7

---
+++ R/sets.R
@@ -0,0 +1,52 @@
+#' Set operations
+#'
+#' Union (`union()`), intersect (`intersect()`), difference (`setdiff()`),
+#' and equality (`setequal()`) for two vectors representing sets. Determine
+#' membership with `is.element()`.
+#'
+#' These functions override the set functions provided in base to make them
+#' generic so that packages can provide methods for different data types. The
+#' default methods call the base versions.
+#'
+#' @param x,y Vectors to combine.
+#' @param el,set Element and set to compare.
+#' @param ... Other arguments passed on to methods.
+#' @return For `union()`, `intersect()`, and `setdiff()`, a vector with all
+#'   duplicate removed.
+#'
+#'   For `setequal()` and `is.element()`,  a logical `TRUE` or `FALSE`.`
+#' @name setops
+#' @examples
+#' intersect(1:5, 4:8)
+#' union(1:5, 4:8)
+#'
+#' setdiff(1:5, 4:8)
+#' setdiff(4:8, 1:5)
+NULL
+
+#' @rdname setops
+#' @export
+intersect <- function(x, y, ...) UseMethod("intersect")
+#' @rdname setops
+#' @export
+union <- function(x, y, ...) UseMethod("union")
+#' @rdname setops
+#' @export
+setdiff <- function(x, y, ...) UseMethod("setdiff")
+#' @rdname setops
+#' @export
+setequal <- function(x, y, ...) UseMethod("setequal")
+#' @rdname setops
+#' @export
+is.element <- function(el, set, ...) UseMethod("setequal")
+
+#' @export
+intersect.default <- function(x, y, ...) base::intersect(x, y, ...)
+#' @export
+union.default <- function(x, y, ...) base::union(x, y, ...)
+#' @export
+setdiff.default <- function(x, y, ...) base::setdiff(x, y, ...)
+#' @export
+setequal.default <- function(x, y, ...) base::setequal(x, y, ...)
+#' @export
+is.element.default <- function(el, set, ...) base::is.element(el, set, ...)
