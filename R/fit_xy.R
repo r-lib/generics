fa44f9249da9245510fba1b45548bd0026515ee4
topepo <mxkuhn@gmail.com>
2018-10-05 14:59:27 -0400

    fit methods

---
+++ R/fit_xy.R
@@ -0,0 +1,13 @@
+#' Estimate model parameters. 
+#' 
+#' Estimates parameters for a given model from a set of data in the form of
+#'  a set of predictors (`x`) and outcome(s) (`y`). 
+#' 
+#' @section Methods:
+#' \Sexpr[stage=render,results=Rd]{generics:::methods_rd("fit_xy")}
+#'
+#' @inheritParams compile
+#' @export
+fit_xy <- function(object, ...) {
+  UseMethod("fit_xy")
+}
