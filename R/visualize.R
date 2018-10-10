c55cdbb7cfe62272c7f2d89a98f3a9add8f3cfc7
topepo <mxkuhn@gmail.com>
2018-10-10 16:54:17 -0400

    switched visualize to x from data

--- R/visualize.R
+++ R/visualize.R
@@ -1,13 +1,13 @@
 #' Visualize a data set or object. 
 #' 
-#' @param data A data frame or other object. 
+#' @param x A data frame or other object. 
 #' @param ... Other arguments passed to methods
 #' 
 #' @section Methods:
 #' \Sexpr[stage=render,results=Rd]{generics:::methods_rd("visualize")}
 #'
 #' @export
-visualize <- function(data, ...) {
+visualize <- function(x, ...) {
   UseMethod("visualize")
 }
 
