9d534ddfdb9a16addbedc30e6321cc92106cc06e
Hadley Wickham <h.wickham@gmail.com>
2021-02-06 10:49:41 -0600

    Require R 3.2

--- tests/testthat/test-s4.R
+++ tests/testthat/test-s4.R
@@ -1,6 +1,5 @@
 test_that("S4 bullets print with no issues", {
   local_load_all("testS4Docs")
-  skip_if_below_r_version("3.2")
 
   expect_snapshot(methods_rd("multi_method"))
 })
@@ -8,8 +7,6 @@ test_that("S4 bullets print with no issues", {
 test_that("S4 and S3 packages can intermingle", {
   local_load_all("testS4Docs")
   local_load_all("testMultiMethod")
-
-  skip_if_below_r_version("3.2")
 
   expect_snapshot(methods_rd("multi_method"))
 })
